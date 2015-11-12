b10c569f83b2b0d61b7160f7d188826aeb087e6b
IlyaAshavskiy <iashavskiy@gmail.com>
2015-11-13 01:13:22 +0300

    add eng-ru ru-eng translate

--- bot.py
+++ bot.py
@@ -3,26 +3,40 @@ import time
 import telebot
 import config
 import telegram
+# import goslate
+# import urllib2
+from textblob import TextBlob
 
 bot = telebot.TeleBot(config.token)
 bot1 = telegram.Bot(config.token)
+# gs = goslate.Goslate()
+
+ # bot.send_message(message.chat.id, u"Конечно господин " + message.from_user.first_name +u", ща всё будет!")
 
 
-#def echo_msg(message):
- #   bot.send_message(message.chat.id, message.text)
 @bot.message_handler(func=lambda message: True, content_types=['text'])
-def suka_msg(message):
-    # print (message.text)
-        if (message.text == u"сука") or (message.text == u"Сука") :
-            bot.send_message(message.chat.id, u"Сам сука")
-            bot.send_message(message.chat.id, text=telegram.Emoji.PILE_OF_POO)
-            bot1.sendPhoto(chat_id=message.chat.id, photo='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ_h_mJXGtErnK_jCaZNppMf92iozTrn1SAlcjSf8e55-p7Difa')
-        if (message.text == u'Привет'):
-            bot.send_message(message.chat.id, u"Ну привет")
-        if (message.text == u'Как дела?'):
-            bot.send_message(message.chat.id, u"Было нормально пока ты не написал")
-        else:
-            bot.send_message(message.chat.id, u"Конечно господин " + message.from_user.first_name +u", ща всё будет!")
+def translate_msg(message):
+    if (len(message.text) > 3):
+        b = TextBlob(unicode(message.text))
+        if (b.detect_language() == "ru"):
+            tr_text = unicode(b.translate(to="en"))
+            bot.send_message(message.chat.id, tr_text)
+        if (b.detect_language() == "en"):
+            tr_text = unicode(b.translate(to="ru"))
+            bot.send_message(message.chat.id, tr_text)
+
+
+
+
+#def main_msg(message):
+#    if (message.text == u"сука") or (message.text == u"Сука"):
+#        bot.send_message(message.chat.id, u"Сам сука")
+#        bot.send_message(message.chat.id, text=telegram.Emoji.PILE_OF_POO)
+#        bot1.sendPhoto(chat_id=message.chat.id, photo='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ_h_mJXGtErnK_jCaZNppMf92iozTrn1SAlcjSf8e55-p7Difa')
+#    if (message.text == u'Привет'):
+#        bot.send_message(message.chat.id, u"Ну привет")
+#    if (message.text == u'Как дела?'):
+#        bot.send_message(message.chat.id, u"Было нормально пока ты не написал")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
